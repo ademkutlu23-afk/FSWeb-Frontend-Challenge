2ba496f21c46d5a6ee18f379c6760c1891639437
ademkutlu23-afk <ademkutlu23@gmail.com>
2026-07-09 21:44:13 +0300

    Hero section eklendi

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,14 +2,31 @@ import { profileData } from "../data/data";
 
 function Hero() {
   return (
-    <section>
-      <h1>{profileData.name}</h1>
-      <h2>{profileData.title}</h2>
-      <p>{profileData.description}</p>
+    <section className="hero">
+      <div className="hero-left">
+        <p className="hero-small-text">Merhaba 👋</p>
 
-      <a href={profileData.github} target="_blank">
-        GitHub
-      </a>
+        <h1 className="hero-title">
+          Ben {profileData.name}. <br />
+          {profileData.title}
+        </h1>
+
+        <p className="hero-description">{profileData.description}</p>
+
+        <div className="hero-buttons">
+          <a className="hero-button" href={profileData.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+
+          <a className="hero-button secondary" href={profileData.linkedin} target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>
+        </div>
+      </div>
+
+      <div className="hero-right">
+        <div className="hero-image-placeholder">Fotoğraf</div>
+      </div>
     </section>
   );
 }
