015d2792020605432e3f75b379cdbcaba5a5d0d4
ademkutlu23-afk <ademkutlu23@gmail.com>
2026-07-09 21:03:35 +0300

    Header ve basicc stiller eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
@@ -5,6 +6,7 @@ import Projects from "./components/Projects";
 function App() {
   return (
     <main>
+      <Header />
       <Hero />
       <Skills />
       <Projects />
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">AK</div>
+
+      <nav className="nav">
+        <button className="nav-button">DARK MODE</button>
+        <span className="divider">|</span>
+        <button className="nav-button">TÜRKÇE</button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
